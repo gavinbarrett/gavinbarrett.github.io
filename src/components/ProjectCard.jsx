a155cecdc6bbdacde604fc6c4374c4103029f897
Gavin <gavinbrrtt@gmail.com>
2021-01-30 00:41:38 -0800

    Changed PhiloSource to substitute MySQL with PostgreSQL.

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
+import React from 'react'
 import ReactTooltip from 'react-tooltip';
 import './sass/ProjectCard.scss';
 
-const ProjectCard = ({applink, githublink, project, image, desc, lang, langdesc, fe, styles}) => {
+const ProjectCard = ({applink, githublink, project, image, desc, lang, langdesc, fe, styles, db}) => {
+	{/* compose a concise description of a project */}
 	return (<div className="projectcard">
 		<div className='projecttitle'>{project}</div>
 		<div className='projectsnippet'>
 		<ProjectDescription desc={desc} applink={applink} githublink={githublink} project={project}/>
-		<ProjectImage image={image} lang={lang} langdesc={langdesc} fe={fe} styles={styles}/>
+		<ProjectImage image={image} lang={lang} langdesc={langdesc} fe={fe} styles={styles} db={db}/>
 		</div>
 	</div>);
 }
 
 const ProjectDescription = ({desc, applink, githublink, project}) => {
+	{/* describe the project's purpose and any novel qualities; link to the application and its source code */}
 	return (<div className='desc'>
 		<div className="descbox">{desc}</div>
 		<div className='links'>
@@ -22,28 +24,37 @@ const ProjectDescription = ({desc, applink, githublink, project}) => {
 	</div>);
 }
 
-const ProjectImage = ({image, lang, langdesc, fe, styles}) => {
+const ProjectImage = ({image, lang, langdesc, fe, styles, db}) => {
+	{/* display an image/gif of the project and display the project's tech stack */}
 	return (<div className='projectimage'>
 		<div className='imagebox'>
 			{image}
 		</div>
-		<ProjectStack lang={lang} langdesc={langdesc} fe={fe} styles={styles}/>
+		<ProjectStack lang={lang} langdesc={langdesc} fe={fe} styles={styles} db={db}/>
 	</div>);
 }
 
-const ProjectStack = ({lang, langdesc, fe, styles}) => {
+const ProjectStack = ({lang, langdesc, fe, styles, db}) => {
+	{/* display an array of technologies used to create the project */}
 	return (<div className='projectstack'>
+		{/* back end language for project */}
 		<img className="lang" src={lang} data-tip={langdesc} width="35px" height="35px"/>
-		<img className="lang" src={fe} width="45px" height="45px"/>
+		{/* front end for project (React) */}
+		{fe ? <img className="lang" src={fe} width="45px" height="45px"/> : ''}
+		{/* styling option for project (CSS3/SCSS) */}
 		{styles ? <img className="lang" src={styles} width="42px" height="42px"/> : ''}
+		{/* database system */}
+		{db ? <img className="lang" src={db} data-tip={langdesc} width="58px" height="54px"/> : ''}
 	</div>);
 }
 
 const AppLink = ({applink, project}) => {
+	{/* link to deployed application; if it isn't deployed we link to the source code */}
 	return (<a className='applink' href={applink}>{project} Demo</a>);
 }
 
 const GithubLink = ({githublink}) => {
+	{/* link to each projects' source code */}
 	return (<a className='githublink' href={githublink}><img className='giticon' src='../dist/GitHub-Mark/PNG/GitHub-Mark-Light-120px-plus0xEAAC8B.png'/>Code</a>);
 }
 
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -6,6 +6,7 @@ import './sass/Table.scss';
 
 const Table = ({donations, updateDonations}) => {
 	
+	// project image/gif paths
 	const organon = <img className='image' src='../../dist/organon_demo.gif'/>;
 	const exif_god = <img className='image' src='../../dist/exif.gif'/>;
 	const secretfracture = <img className='image' src='../../dist/secret.gif'/>;
@@ -14,7 +15,18 @@ const Table = ({donations, updateDonations}) => {
 	const protophile = <img className='image' src='../../dist/protophile.gif'/>
 	const bipsuite = <img className='image' src='../../dist/bipsuite.png'/>
 	const philsource = <img className='image' src='../../dist/philsource.png'/>
+	
+	// stack logo paths
+	const shell = '../../dist/shelllogo.png';
+	const cli = '../../dist/clilogo.png';
+	const node = '../../dist/nodelogo.png';
+	const python = '../../dist/pythonlogo.png';
+	const react = '../../dist/react.png';
+	const sass = '../../dist/sass.png';
+	const css = '../../dist/css.png';
+	const psql = '../../dist/post.png';
 
+	// project descriptions
 	const exifdesc = 'Exif God is a simple utility for viewing and removing image metadata.';
 	const organondesc = 'Organon is a logic analyzer that determines whether arguments in propositional logic are valid.';
 	const secretdesc = 'Secret Fracture is a tool that implements a thresholded secret sharing scheme, encrypting messages into shares.';
@@ -27,23 +39,16 @@ const Table = ({donations, updateDonations}) => {
 	const bipdesc = 'Bip Suite is an implementation of the BIP32/BIP39 protocol for generating and managing Bitcoin HD wallets. Currently, the program can generate cryptographically secure BIP32 mnemonic seeds and derive the associated BIP39 extended key pair associated with that seed.';
 	const phildesc = 'PhiloSource is a web archive for philosophical texts that supports document searching and community interaction.'
 
-	const shell = '../../dist/shelllogo.png';
-	const node = '../../dist/nodelogo.png';
-	const python = '../../dist/pythonlogo.png';
-	const react = '../../dist/react.png';
-	const sass = '../../dist/sass.png';
-	const css = '../../dist/css.png';
-
 	return (<div id='portblock'>
 	<div className='tabheader'>{'Projects'}</div>
 	<div id='table'>
-	<ProjectCard project={'PhiloSource'} image={philsource} desc={phildesc} applink={'https://github.com/gavinbarrett/PhilSource'} githublink={'https://github.com/gavinbarrett/PhilSource'} lang={node} langdesc={'Node.js'} fe={react} styles={sass}/>
+	<ProjectCard project={'PhiloSource'} image={philsource} desc={phildesc} applink={'https://github.com/gavinbarrett/PhilSource'} githublink={'https://github.com/gavinbarrett/PhilSource'} lang={node} langdesc={'Node.js'} fe={react} styles={sass} db={psql}/>
 	<ProjectCard project={'CA Covid Dash'} image={covdash} desc={covidashdesc} applink={'https://coviddash.site'} githublink={'https://github.com/gavinbarrett/CACovidDash'} lang={python} langdesc={'Python3'} fe={react} styles={sass}/>
 	<ProjectCard project={'Organon'} image={organon} desc={organondesc} applink={'http://organon-engine.herokuapp.com/'} githublink={'https://github.com/gavinbarrett/SL_Engine'} lang={python} langdesc={'Python3'} fe={react} styles={css}/>
 	<ProjectCard project={'D&Dice'} image={dndice} desc={dndicedesc} applink={'https://dndice.site/'} githublink={'https://github.com/gavinbarrett/DnDice'} lang={node} langdesc={'Node.js'} fe={react} styles={css}/>
 	<ProjectCard project={'Exif God'} image={exif_god} desc={exifdesc} applink={'http://exif-god.herokuapp.com/'} githublink={'https://github.com/gavinbarrett/exif_god'} lang={python} langdesc={'Python3'} fe={react} styles={css}/>
-	<ProjectCard project={'ProtoPhile'} image={protophile} desc={protophiledesc} applink={'https://github.com/gavinbarrett/ProtoPhile/'} githublink={'https://github.com/gavinbarrett/ProtoPhile/'} lang={python} langdesc={'Python3'} fe={shell} styles={null}/>
-	<ProjectCard project={'Bip Suite'} image={null} desc={bipdesc} applink={'https://github.com/gavinbarrett/BIP39_Suite/'} githublink={'https://github.com/gavinbarrett/BIP39_Suite/'} image={bipsuite} lang={python} langdesc={'Python3'} fe={shell} styles={null}/>
+	<ProjectCard project={'ProtoPhile'} image={protophile} desc={protophiledesc} applink={'https://github.com/gavinbarrett/ProtoPhile/'} githublink={'https://github.com/gavinbarrett/ProtoPhile/'} lang={python} langdesc={'Python3'} fe={null} styles={null}/>
+	<ProjectCard project={'Bip Suite'} image={null} desc={bipdesc} applink={'https://github.com/gavinbarrett/BIP39_Suite/'} githublink={'https://github.com/gavinbarrett/BIP39_Suite/'} image={bipsuite} lang={python} langdesc={'Python3'} fe={null} styles={null}/>
 	</div>
 	<div className='tabheader'>Contact Me</div>
 	{donations ? <Donations updateDonations={updateDonations}/> : ''}
