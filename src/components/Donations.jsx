b8fc26c4bcd7ceb0f43ad810a8ac2e84273f616d
Gavin <gavinbrrtt@gmail.com>
2021-05-15 11:28:12 -0700

    Removed Bitcoin address from the UI. Added addresses for ALGO and XLM along with a text snippet asking for energy efficient PoS coin donations.

--- src/components/Donations.jsx
+++ src/components/Donations.jsx
@@ -5,6 +5,9 @@ export const Donations = ({displayed}) => {
 	const venmo = '@Gavin-Barrett-4';
 	const cashapp = '$GavinABarrett';
 	const bitcoin = '3J9nH7hYiodP6xX1eTkYwd3KJuM3hfvRph';
+	const algo = '2LG3OYODNOER2KB4OQ7DTUEZD3AENEV2BESE3R7J7VEH2CDMANYJRPXR5M';
+	const xlm = 'GDPONGJGI7DMGRNA3EHLL2XB3P4L6BMS5JXGXTZSOKYQQ3NXAI47STRF';
+	const ada = 'addr1q8rd14dety86d0awqj65kcnn2mrgcv8vnhtj1yw3yy3t2fy9vumw70p5csamzacf1w6pddxd91xvmtx6yh3f4mqza2usxdkrmu';
 	const [copyState, updateCopyState] = useState("Copy");
 	const revertClipboard = async () => {
 		await updateCopyState("Copy");
@@ -20,8 +23,17 @@ export const Donations = ({displayed}) => {
 	return (<div className={`donationlinks ${displayed}`}>
 		<div className="payment"><img className='donatelogo' src='../../dist/cashapplogot.png'/>{cashapp}</div>
 		<div className="payment"><img className='donatelogo' src='../../dist/venmologo.png'/>{venmo}</div>
-		<div className="payment"><img className='donatelogo' src='../../dist/bitcoinlogo.png'/>
-			{bitcoin}
+		<p className="crypto-disclaimer">
+			{"If you'd like to send me cryptocurrency, I'd really appreciate some energy-efficient, PoS crypto! I'm a big fan of both ALGO and XLM."}
+		</p>
+		<div className="payment"><img className="donatelogo" src="../../dist/algologo.png"/>
+			{algo}
+			<div className="clipboard-box" onClick={copyToClipboard}>
+				<p className="copy-address">{copyState}</p>
+			</div>
+		</div>
+		<div className="payment"><img className="donatelogo" src="../../dist/xlmlogo.png"/>
+			{xlm}
 			<div className="clipboard-box" onClick={copyToClipboard}>
 				<p className="copy-address">{copyState}</p>
 			</div>
