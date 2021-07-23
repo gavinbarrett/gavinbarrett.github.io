7de900d769e6e10911e71d424f1936233a011d10
Gavin <gavinbrrtt@gmail.com>
2021-07-23 16:03:58 -0700

    Fixed separating out the different Carousel components.

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -17,45 +17,47 @@ const Input = styled.input`
 	}
 `;
 
+const Form = styled.form`
+	&[name=${props => props.formname}] {
+		width: 40vw;
+		height: 50vh;
+		top: 0;
+		left: 0;
+		bottom: 0;
+		right: 0;
+		white-space: nowrap;
+		overflow: hidden;
+	}
+	&[name=${props => props.formname}] input {
+		position: absolute;
+		z-index: -1;
+		display: none;
+	}
+	&[name=${props => props.formname}] label  {
+		color: #fff;
+		transition: transform 400ms ease-out;
+		display: inline-block;
+		min-height: 100%;
+		width: 45vw;
+		height: 50vh;
+		position: relative;
+		z-index: 1;
+		text-align: center;
+		line-height: 100vh;
+		background-size: cover;
+		background-position: center center;
+		font-size: 3rem;
+		border-radius: 25px;
+	}
+`;
+
 export const Carousel = ({images, set, genidx}) => {
 	return (<div className={'container'}>
-		<form id={`form-${set}`}>
-			{images ? images.map((path, index) => <Input key={index + genidx} name={`image-${set}`} type={"radio"} id={`image-${set}-${index}`} num={index + genidx} shift={100 * index}/>) : ''}
+		<Form name={`form-${set}`} formname={`form-${set}`}>
+			{images ? images.map((path, index) => <Input key={index + genidx} name={`image-${set}`} type={"radio"} id={`image-${set}-${index}`} num={index} shift={100 * index}/>) : ''}
 			{images ? images.map((path, index) => {
 				return <StyledLabel path={path} key={index + genidx} for={`image-${set}-${index}`} label_name={`image-${set}-${index}`}/>
 			}) : ''}
-		</form>
+		</Form>
 	</div>);
 }
-
-
-/*
-const StyledLabel = styled.label`
-	&[for=${props => props.label_name}] {
-		background-image: url(assets/tunnelr.png);
-	}
-`;
-
-const Input = styled.input`
-	&:nth-of-type(${props => props.num + 1}):checked ~ label:nth-of-type(${props => props.num + 1}) { 
-	   z-index: 0;
-	}
-	&:nth-of-type(${props => props.num + 1}):checked ~ label {
-		transform: translate3d(-${props => props.shift}%, 0, 0);
-	}
-`;
-
-export const Carousel = ({images, set, genidx}) => {
-	return (<div className={'container'}>
-		<form>
-			{images ? images.map((path, index) => {
-				return <Input key={index} type={"radio"} name={`image`} id={`car${index}`} num={index + genidx} shift={100 * index}/>
-			}) : ''}
-			{images ? images.map((path, index) => {
-				console.log(`Setting path: ${path}\nfor label_name: ${`image-${set}-${index}`}`);
-				return <StyledLabel key={index} for={`car${index}`} path={'../../dist/tunnelr.png'} label_name={`car${index}`}/>
-			}) : ''}
-		</form>
-	</div>);
-}
-*/
